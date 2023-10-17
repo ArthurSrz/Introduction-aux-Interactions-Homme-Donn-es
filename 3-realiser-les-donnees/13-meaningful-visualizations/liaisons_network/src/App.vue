e4e8a5b2f83687fd7cb84de8a2ca7120e755e189
ArthurSrZ <arthur@datactivi.st>
2023-10-17 14:56:47 +0200

    new

---
+++ 3-realiser-les-donnees/13-meaningful-visualizations/liaisons_network/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <NetworkData />
+  </div>
+</template>
+
+<script>
+import NetworkData from 'components/NetworkData.vue'
+
+export default {
+  components: {
+    NetworkData,
+  },
+}
+</script>
+
+<style>
+/* Ajoutez des styles CSS globaux ici */
+</style>
